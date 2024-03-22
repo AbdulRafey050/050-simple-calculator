a0c30786703741611f30b5f24978cf73f73b6fb0
AbdulRafey050 <abdulrafeyansari29@gmail.com>
2024-03-22 16:28:36 +0500

    push my simple calculator

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message:"enter your first number", type:"number", name:"firstnumber"},
+    {message:"enter your second number", type:"number", name:"secondnumber"},
+    {message:"select one of operator to perform operation " ,type:"list", name:"operator",choices:["addition","subtraction","multiplication","division"]},
+
+]);
+// conditonal statement :
+if(answer.operator==="addition"){
+    console.log(answer.firstnumber+answer.secondnumber);
+}
+else if(answer.operator==="subtraction"){
+    console.log(answer.firstnumber-answer.secondnumber);
+}
+else if(answer.operator==="multiplication"){
+    console.log(answer.firstnumber*answer.secondnumber);
+}
+else if(answer.operator==="division"){
+    console.log(answer.firstnumber/answer.secondnumber);
+}
+else{
+    console.log("please select valid operator");
+}
